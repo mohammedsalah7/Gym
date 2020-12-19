f140dbecfd111aa8569cfe9ba387268f5ef354bc
mohammedsalah7 <mhamad_m@windowslive.com>
2020-12-19 23:53:25 +0200

    add button refresh inside signup page

--- src/Page/Signup/index.jsx
+++ src/Page/Signup/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Forms from "./form";
 import Back from "../../Components/Back";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
+import * as S from "./style";
 export default function Signup() {
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
     <>
+      <S.Refresh onClick={refreshPage}>
+        {" "}
+        <S.Icon icon={faSync} />
+      </S.Refresh>
       <Back />
       <Forms />
     </>
